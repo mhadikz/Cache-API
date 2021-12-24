9205fd28af9448e8e9f47659576ef4bedee48ba3
mhadikz <mhkaramzadeh7@gmail.com>
2021-12-24 14:30:38 +0330

    Create or update a cache data

---
+++ src/controllers/cache.controller.ts
@@ -0,0 +1,32 @@
+import { BaseController } from './base.controller'
+import { Request, Response } from 'express'
+import { CacheRepo } from '../database/repository/cache.repo'
+const repo = new CacheRepo()
+
+/**
+ * Class to manage Cache data
+ */
+export class CacheController extends BaseController {
+
+   constructor() {
+      super()
+   }
+
+   /**
+    * This method returns cached data or creates a random string if the key doesn't exist
+    * @param  {Request} req
+    * @param  {Response} res
+    */
+   async getCacheData(req: Request, res: Response) {
+      try {
+         const key = req.params.key
+
+         if (!key) return super.badRequest(res, 'Key is required.')
+
+         const result = await repo.getData(key)
+         return super.ok(res, 'Data was created.', result)
+      } catch (error) {
+         return super.fail(res, error)
+      }
+   }
+}
