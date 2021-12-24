c1511284b5140f6813516df8ea6977c06ea2ef15
mhadikz <mhkaramzadeh7@gmail.com>
2021-12-24 16:54:12 +0330

    Add mock data

---
+++ src/tests/__mocks__/cache.mocks.ts
@@ -0,0 +1,13 @@
+export const mocks = [
+    {
+       key: 'key-test-1',
+       value: {
+           webTitle: "test-title",
+           redirectUrl: "google.com",
+    },
+    },
+    {
+        key: 'key-test-2',
+        value: 'dlskfjdelgj483lkdsjfldsfk0lkmv09-65',
+     }
+]
